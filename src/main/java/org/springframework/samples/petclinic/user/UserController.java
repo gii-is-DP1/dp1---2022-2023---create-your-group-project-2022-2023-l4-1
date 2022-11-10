871856cc557663a0af28be01f0da45cb0bc688cf
ignaciowarleta <nachowarleta00@gmail.com>
2022-11-10 11:41:29 +0100

    UserController

--- src/main/java/org/springframework/samples/petclinic/user/UserController.java
+++ src/main/java/org/springframework/samples/petclinic/user/UserController.java
@@ -20,6 +20,7 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.jugador.Jugador;
 import org.springframework.samples.petclinic.owner.Owner;
 import org.springframework.samples.petclinic.owner.OwnerService;
 import org.springframework.stereotype.Controller;
@@ -30,21 +31,19 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
- * @author Juergen Hoeller
- * @author Ken Krebs
- * @author Arjen Poutsma
- * @author Michael Isvy
+ * @author Ignacio Warleta
+ * @author Gabriel Vacaro
  */
 @Controller
 public class UserController {
 
-	private static final String VIEWS_OWNER_CREATE_FORM = "users/createOwnerForm";
+	private static final String VIEWS_PLAYERS_CREATE_FORM = "user/createOrUpdateUserForm";
 
-	private final OwnerService ownerService;
+	private final JugadorService jugadorService;
 
 	@Autowired
-	public UserController(OwnerService clinicService) {
-		this.ownerService = clinicService;
+	public UserController(JugadorService ps) {
+		this.jugadorService = ps;
 	}
 
 	@InitBinder
@@ -54,19 +53,21 @@ public class UserController {
 
 	@GetMapping(value = "/users/new")
 	public String initCreationForm(Map<String, Object> model) {
-		Owner owner = new Owner();
-		model.put("owner", owner);
-		return VIEWS_OWNER_CREATE_FORM;
+		Jugador jugador = new Jugador();
+		User user = new User();
+		jugador.setUser(user);
+		model.put("jugador", jugador);
+		return VIEWS_PLAYERS_CREATE_FORM;
 	}
 
 	@PostMapping(value = "/users/new")
-	public String processCreationForm(@Valid Owner owner, BindingResult result) {
+	public String processCreationForm(@Valid Jugador jugador, BindingResult result) {
 		if (result.hasErrors()) {
-			return VIEWS_OWNER_CREATE_FORM;
+			return VIEWS_PLAYERS_CREATE_FORM;
 		}
 		else {
 			//creating owner, user, and authority
-			this.ownerService.saveOwner(owner);
+			this.jugadorService.saveJugador(jugador);
 			return "redirect:/";
 		}
 	}
