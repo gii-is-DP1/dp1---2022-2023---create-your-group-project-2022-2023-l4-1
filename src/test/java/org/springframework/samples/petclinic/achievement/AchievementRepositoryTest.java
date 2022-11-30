29f06ca764d58d928971f1d9efe64be9b8d89613
Manpalpin02 <manuel18602@gmail.com>
2022-11-30 21:10:44 +0100

    Mas tests AchievementRepository

--- src/test/java/org/springframework/samples/petclinic/achievement/AchievementRepositoryTest.java
+++ src/test/java/org/springframework/samples/petclinic/achievement/AchievementRepositoryTest.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.achievement;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
@@ -24,6 +25,21 @@ public class AchievementRepositoryTest {
         assertNotNull(achievements);
         assertFalse(achievements.isEmpty());
         assertEquals(10, achievements.size());
+    }
+
+    @Test
+    public void findAchievementByNameSuccessTest(){
+        List<Achievement> achievements = achievementRepository.findByName("Acaparador");
+        assertNotNull(achievements);
+        assertFalse(achievements.isEmpty());
+        assertEquals(1, achievements.size());
+    }
+
+    @Test
+    public void findAchievementByNameFailTest(){
+        List<Achievement> achievements = achievementRepository.findByName("PruebaTest");
+        assertNotNull(achievements);
+        assertTrue(achievements.isEmpty());
     }
     
 }
