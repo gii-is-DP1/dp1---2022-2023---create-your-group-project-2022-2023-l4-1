ca2a55fa1b0900101a7f1e05030de8dd1acb37ac
davcorrom <davcorrom@alum.us.es>
2022-12-06 21:31:11 +0100

    Join a match as second or third player.

--- src/main/java/org/springframework/samples/petclinic/jugador/JugadorService.java
+++ src/main/java/org/springframework/samples/petclinic/jugador/JugadorService.java
@@ -1,16 +1,10 @@
 package org.springframework.samples.petclinic.jugador;
 
-
-
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.user.AuthoritiesService;
 import org.springframework.samples.petclinic.user.User;
-import org.springframework.samples.petclinic.user.UserService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class JugadorService {
 
     private JugadorRepository jugadorRepository;	
-	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private AuthoritiesService authoritiesService;
 
 	@Autowired
 	public JugadorService(JugadorRepository jugadorRepository) {
@@ -55,24 +43,5 @@ public class JugadorService {
 
 		jugadorRepository.save(jugador);
 	}
-
-	public User getUserLogged() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = "";
-		if (auth!=null) {
-			if (auth.isAuthenticated() && auth.getPrincipal() instanceof org.springframework.security.core.userdetails.User) {
-				org.springframework.security.core.userdetails.User userLogged = (org.springframework.security.core.userdetails.User) auth.getPrincipal();
-				username = userLogged.getUsername();
-			}
-		}
-        return userService.findUser(username).get();
-    }
-
-	/*@Transactional
-	public void saveJugador(Jugador jugador) throws DataAccessException {
-		jugadorRepository.save(jugador);		
-		userService.saveUser(jugador.getUser());
-		authoritiesService.saveAuthorities(jugador.getUser().getUsername(), "player");
-	}		*/
     
 }
