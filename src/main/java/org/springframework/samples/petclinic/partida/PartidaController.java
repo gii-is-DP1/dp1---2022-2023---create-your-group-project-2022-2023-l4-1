ddc7ecd962f8e08769d8117c97074f72b115414c
davcorrom <davcorrom@alum.us.es>
2022-12-12 13:30:54 +0100

    Show only the games with 2 or less players.

--- src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
+++ src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
@@ -81,7 +81,9 @@ public class PartidaController {
         List<Partida> partidas = new ArrayList<Partida>();
 
         for (Partida partida: partidaService.getPartidas()){
-            if (partida.getFaseActual() == Fase.INICIANDO) partidas.add(partida);
+            if (partida.getFaseActual() == Fase.INICIANDO) {
+                if (partida.getUsersOnTheGame().size() < 3) partidas.add(partida);
+            }
         }
 
         res.addObject("partidas", partidas);
