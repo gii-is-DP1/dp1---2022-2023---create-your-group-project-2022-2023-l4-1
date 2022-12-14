920fdbf97a742498b377c5cc178be20d147178db
gabrielvcg <gabrivacaro@gmail.com>
2022-12-14 13:54:05 +0100

    Cartas de la montaña

--- src/main/java/org/springframework/samples/petclinic/CartaDeLaMontana/CartaDeLaMontanaRepository.java
+++ src/main/java/org/springframework/samples/petclinic/CartaDeLaMontana/CartaDeLaMontanaRepository.java
@@ -1,8 +1,24 @@
 package org.springframework.samples.petclinic.CartaDeLaMontana;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartaDeLaMontanaRepository extends CrudRepository<CartaDeLaMontana, Integer>{
     
+    
+/* 
+    @Query("SELECT card FROM MountainCard card WHERE card.group =:cardgroup")
+    List<CartaDeLaMontana> findByGroupCards(@Param("cardgroup") Integer cardGroup) throws DataAccessException;
+
+    @Query("SELECT card FROM MountainCard card WHERE card.group = 1 AND card.xPosition =:xposition AND card.yPosition=:yposition")
+    CartaDeLaMontana findInitialCardByPosition(@Param("posicion") Integer posicion);
+*/
 }
