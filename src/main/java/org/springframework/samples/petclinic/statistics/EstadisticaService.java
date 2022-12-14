9e50fde9b63c2a09c368e06a1abd586692dd4e81
danguepre <danguepre@alum.us.es>
2022-12-14 14:42:34 +0100

    estadisticas updated

--- src/main/java/org/springframework/samples/petclinic/statistics/EstadisticaService.java
+++ src/main/java/org/springframework/samples/petclinic/statistics/EstadisticaService.java
@@ -1,11 +1,13 @@
 package org.springframework.samples.petclinic.statistics;
 
+import java.time.Duration;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.user.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -35,19 +37,29 @@ public class EstadisticaService {
     }
     
     @Transactional
-    public void saveStatistics(Estadistica estadisticas) throws DataAccessException {
-        estadisticaRepository.save(estadisticas);
+    public void newStatistics(User user) throws DataAccessException {
+        Estadistica estadisticas = new Estadistica();
+        estadisticas.setTiempoJugado(Duration.ofSeconds(1234));
+		estadisticas.setPartidasJugadas(0);
+		estadisticas.setPartidasGanadas(0);
+		estadisticas.setHierroTotal(0);
+		estadisticas.setOroTotal(0);
+		estadisticas.setAceroTotal(0);
+		estadisticas.setObjetosTotal(0);
+		estadisticas.setMedallasTotal(0);
+        estadisticas.setUser(user);
+		estadisticaRepository.save(estadisticas);
     }
     //Actualiza las estadisticas al finalizar partida
     /*  
     @Transactional
-    public void updateStatistics(PlayerState playerState, Estadistica estadisticas, boolean winner) {
+    public void updateStatistics(Jugador jugador, Estadistica estadisticas, boolean winner) {
         if (winner) estadisticas.partidasGanadas++;
-        estadisticas.setOroTotal(estadisticas.oroTotal + playerState.getGold());
-        estadisticas.setAceroTotal(estadisticas.aceroTotal + playerState.getSteel());
-        estadisticas.setObjetosTotal(estadisticas.hierroTotal + playerState.getObject());
-        estadisticas.setHierroTotal(estadisticas.objetosTotal + playerState.getIron());
-        estadisticas.setMedallasTotal(estadisticas.medallasTotal + playerState.getMedal());
+        estadisticas.setOroTotal(estadisticas.oroTotal + jugador.getGold());
+        estadisticas.setAceroTotal(estadisticas.aceroTotal + jugador.getSteel());
+        estadisticas.setObjetosTotal(estadisticas.hierroTotal + jugador.getObject());
+        estadisticas.setHierroTotal(estadisticas.objetosTotal + jugador.getIron());
+        estadisticas.setMedallasTotal(estadisticas.medallasTotal + jugador.getMedal());
         estadisticas.setPartidasJugadas(estadisticas.partidasJugadas++);
         estadisticaRepository.save(estadisticas);
     }
