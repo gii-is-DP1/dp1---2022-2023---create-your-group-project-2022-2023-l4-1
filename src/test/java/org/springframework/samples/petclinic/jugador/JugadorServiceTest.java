5c43533b05897c28fd22b800647392776d30df2d
Manpalpin02 <manuel18602@gmail.com>
2023-01-11 17:09:03 +0100

    More Service Tests

--- src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTest.java
@@ -2,9 +2,11 @@ package org.springframework.samples.petclinic.jugador;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Collection;
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.samples.petclinic.partida.Partida;
+import org.springframework.samples.petclinic.partida.PartidaRepository;
 import org.springframework.samples.petclinic.partida.enums.NumRondas;
 import org.springframework.samples.petclinic.user.User;
 
@@ -22,6 +25,11 @@ public class JugadorServiceTest {
 
     @Autowired
     JugadorRepository jugadorRepository;
+
+    @Autowired
+    PartidaRepository partidaRepository;
+
+
 
     @Test
     public void saveSuccessfulTest(){
@@ -54,8 +62,10 @@ public class JugadorServiceTest {
         assertThrows(Exception.class, () -> jugadorService.save(user2, partida2));
     }
 
+
+
     @Test
-    public void findJugadorByIdTest(){
+    public void findJugadorByIdSuccessfulTest(){
         JugadorService jugadorService = new JugadorService(jugadorRepository);
         Jugador jugador = jugadorService.findJugadorById(1);
         assertNotNull(jugador);
@@ -64,7 +74,16 @@ public class JugadorServiceTest {
     }
 
     @Test
-    public void getJugadoresTest(){
+    public void findJugadorByIdUnsuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        Jugador jugador = jugadorService.findJugadorById(1304);
+        assertNull(jugador);
+    }
+
+
+
+    @Test
+    public void getJugadoresSuccessfulTest(){
         JugadorService jugadorService = new JugadorService(jugadorRepository);
         List<Jugador> jugadores = jugadorService.getJugadores();
         assertNotNull(jugadores);
@@ -73,7 +92,18 @@ public class JugadorServiceTest {
     }
 
     @Test
-    public void findOwnerByLastNameTest(){
+    public void getJugadoresUnsuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        List<Jugador> jugadores = jugadorService.getJugadores();
+        assertNotNull(jugadores);
+        assertFalse(jugadores.isEmpty());
+        assertNotEquals(1254, jugadores.size());
+    }
+
+
+    
+    @Test
+    public void findOwnerByLastNameSuccessfulTest(){
         JugadorService jugadorService = new JugadorService(jugadorRepository);
         Collection<Jugador> jugadores = jugadorService.findOwnerByLastName("Cortabarra Romero");
         assertNotNull(jugadores);
@@ -82,11 +112,47 @@ public class JugadorServiceTest {
     }
 
     @Test
-    public void deleteByIdTest(){
+    public void findOwnerByLastNameUnsuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        Collection<Jugador> jugadores = jugadorService.findOwnerByLastName("Palacios Pineda");
+        assertNotNull(jugadores);
+        assertTrue(jugadores.isEmpty());
+    }
+
+
+
+    @Test
+    public void deleteByIdSuccessfulTest(){
         JugadorService jugadorService = new JugadorService(jugadorRepository);
         jugadorService.deleteJugadorById(1);
         assertNull(jugadorService.findJugadorById(1));
         assertEquals(5, jugadorService.getJugadores().size());
+    }
+
+    @Test
+    public void deleteByIdUnsuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        assertThrows(Exception.class, () -> jugadorService.deleteJugadorById(35));
+    }
+
+
+
+    @Test
+    public void findJugadorInAGameSuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        Partida partida = partidaRepository.findById(1);
+        Jugador jugador = jugadorService.findJugadorInAGame("pabmarval", partida);
+        assertNotNull(jugador);
+        assertEquals(15, jugador.getCantidadHierro());
+        assertEquals(1, jugador.getId());
+    }
+
+    @Test
+    public void findJugadorInAGameUnsuccessfulTest(){
+        JugadorService jugadorService = new JugadorService(jugadorRepository);
+        Partida partida = partidaRepository.findById(1);
+        Jugador jugador = jugadorService.findJugadorInAGame("Manpalpin02", partida);
+        assertNull(jugador);
     }
     
 }
